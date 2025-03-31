bf3b36bf88217a1e782addb884e7478f4568e39f
A先生 <142523398+axs66@users.noreply.github.com>
2025-03-31 22:25:23 +0800

    Add files via upload

---
+++ WeChatNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeChatNotification
+//
+//  Created by hbb on 2025/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
