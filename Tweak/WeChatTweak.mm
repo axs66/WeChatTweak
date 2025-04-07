f4be507d69ee64ca61ea447569e8d1243c06117f
A先生 <142523398+axs66@users.noreply.github.com>
2025-04-07 23:19:42 +0800

    Update WeChatTweak.mm

--- Tweak/WeChatTweak.mm
+++ Tweak/WeChatTweak.mm
@@ -3,9 +3,18 @@
 #import <objc/runtime.h>
 #import <objc/message.h>
 
-// 配置管理器接口声明（新增）
+// 配置管理器接口声明
 @interface WTConfigManager : NSObject
 + (BOOL)isAntiRevokeEnabled;
+@end
+
+// 配置管理器实现
+@implementation WTConfigManager
+
++ (BOOL)isAntiRevokeEnabled {
+    return [[NSUserDefaults standardUserDefaults] boolForKey:@"enableAntiRevoke"];
+}
+
 @end
 
 // 保存原始函数指针
@@ -13,27 +22,24 @@ static void (*original_onRevokeMessage)(id, SEL, id);
 
 // 自定义消息撤回拦截逻辑
 static void new_onRevokeMessage(id self, SEL _cmd, id msg) {
-    // 动态加载 WTConfigManager 类并检查是否启用防撤回
     Class wtConfigClass = NSClassFromString(@"WTConfigManager");
     if (wtConfigClass) {
         SEL antiRevokeSelector = NSSelectorFromString(@"isAntiRevokeEnabled");
         if ([wtConfigClass respondsToSelector:antiRevokeSelector]) {
             BOOL isEnabled = ((BOOL (*)(id, SEL))objc_msgSend)(wtConfigClass, antiRevokeSelector);
             if (isEnabled) {
-                // 启用防撤回，阻止撤回并高亮显示
                 NSLog(@"[WeChatTweak] 拦截撤回消息: %@", msg);
                 return;
             }
         }
     }
     
-    // 如果未启用防撤回，则执行原始撤回逻辑
+    // 执行原始撤回逻辑
     ((void(*)(id, SEL, id))original_onRevokeMessage)(self, _cmd, msg);
 }
 
 // 自定义微信多开逻辑
 static void tweak_launchNewInstance(void) {
-    // 安全的URL打开方式
     NSURL *url = [NSURL URLWithString:@"wechat://"];
     if ([[UIApplication sharedApplication] canOpenURL:url]) {
         [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
@@ -60,14 +66,13 @@ void swizzleMethod(Class cls, SEL originalSel, SEL swizzledSel) {
     }
 }
 
-// 配置管理器实现（新增）
-@implementation WTConfigManager
-+ (BOOL)isAntiRevokeEnabled {
-    return YES; // 默认启用防撤回
-}
-@end
-
 __attribute__((constructor)) static void tweak_init() {
+    // 确保在 NSUserDefaults 中有默认设置
+    if (![[NSUserDefaults standardUserDefaults] objectForKey:@"enableAntiRevoke"]) {
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"enableAntiRevoke"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+    
     // 替换消息撤回方法
     Class messageServiceCls = objc_getClass("MessageService");
     swizzleMethod(messageServiceCls,
