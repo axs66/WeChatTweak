99a40de8fe2930909fd34cfc7b31c4614b3264b6
A先生 <142523398+axs66@users.noreply.github.com>
2025-04-07 21:07:22 +0800

    Update WeChatTweak.mm

--- Tweak/WeChatTweak.mm
+++ Tweak/WeChatTweak.mm
@@ -1,6 +1,9 @@
 #import "WeChatTweak.h"
 #import <objc/runtime.h>
-#import "fishhook.h"
+#import <objc/message.h>
+
+// 如果你要用 fishhook，可以保留，否则可以删除 fishhook 的引用
+//#import "fishhook.h"
 
 // 替换微信原始方法
 static void (*original_onRevokeMessage)(id, SEL, id);
@@ -10,26 +13,23 @@ static void new_onRevokeMessage(id self, SEL _cmd, id msg) {
         return;
     }
     // 自定义逻辑：阻止撤回并高亮显示
-    NSLog(@"拦截撤回消息: %@", msg);
-}
-
-// 多开逻辑
-static void tweak_launchNewInstance(id self, SEL _cmd) {
-    NSWorkspace *workspace = [NSWorkspace sharedWorkspace];
-    [workspace launchApplication:@"/Applications/WeChat.app"];
+    NSLog(@"[WeChatTweak] 拦截撤回消息: %@", msg);
 }
 
 // 初始化 Hook
 __attribute__((constructor)) static void tweak_init() {
-    // 替换消息撤回方法
     Class cls = objc_getClass("MessageService");
-    method_exchangeImplementations(
-        class_getInstanceMethod(cls, @selector(onRevokeMessage:)),
-        class_getInstanceMethod(cls, @selector(tweak_onRevokeMessage:))
-    );
-    
-    // 替换多开方法
-    rebind_symbols((struct rebinding[1]){
-        {"_Z15CreateNewInstancev", (void*)tweak_launchNewInstance, (void**)&original_CreateNewInstance}
-    }, 1);
+    if (cls && class_getInstanceMethod(cls, @selector(onRevokeMessage:))) {
+        Method origMethod = class_getInstanceMethod(cls, @selector(onRevokeMessage:));
+        
+        // 保留原方法实现地址
+        original_onRevokeMessage = (void *)method_getImplementation(origMethod);
+        
+        // 替换为新方法
+        method_setImplementation(origMethod, (IMP)new_onRevokeMessage);
+        
+        NSLog(@"[WeChatTweak] onRevokeMessage: Hook 成功");
+    } else {
+        NSLog(@"[WeChatTweak] ❌ 找不到 MessageService 或 onRevokeMessage: 方法");
+    }
 }
